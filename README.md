b8cf1ea1b1156a90bb0ad43f556aac45898ff9aa
Sedin Ćeman <100443755+KloniraniDecak@users.noreply.github.com>
2022-04-01 21:58:44 +0200

    Create single game function. Tested

--- README.md
+++ README.md
@@ -3,3 +3,31 @@ Odin Project Excercise
 
 Create JavaScript Rock Paper Scissor game
 
+Problem:
+Create simple JavaScript game of Rock Paper Scissors. 
+
+Description:
+Create code that simulates game of Rock Paper Scissor.
+Player enter his choice which is compared with computer random choice.
+As a result player cna win, loose or draw game.
+
+Interface:
+Empty html with externally linked js
+For now we will play from console
+
+Inputs:
+Player enters his choice: Rock, Paper or Scissors (case insensitive)
+Computer gets random choice between Rock, Paper and Scissors
+
+Outputs:
+Player can get three different output results: WIN / LOOSE / DRAW
+
+Pseudocode:
+Function computerPlay that assigns value to computerSelection.
+Value can be: rock / paper / scissors
+Prompt for Player selection and assign value to playerSelection
+Value can be: rock / paper / scissors
+    Values are case incensitive
+Compare two values and determine result using nested if else
+
+
--- myScript.js
+++ myScript.js
@@ -1,1 +1,48 @@
 console.log("Hello World");
+
+
+let myArray = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay () {
+    let computerSelection = Math.floor(Math.random() * myArray.length);
+    return (computerSelection, myArray[computerSelection]);
+}
+
+
+function playRound (playerSelection, computerSelection) {
+    console.log ("You: " + playerSelection)
+    console.log("Computer: " + computerSelection);
+
+    if (playerSelection === "rock" && computerSelection === "Rock") {
+        console.log("Draw");
+        } else if (playerSelection === "rock" && computerSelection === "Paper") {
+            console.log("You lose! " + computerSelection + " beats " + playerEnter);
+        } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+            console.log("You win! " + playerEnter + " beats " +computerSelection);  
+    }
+     else if (playerSelection === "paper" && computerSelection === "Paper") {
+        console.log("Draw");
+        } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+            console.log("You lose! " + computerSelection + " beats " + playerEnter);
+        } else if (playerSelection === "paper" && computerSelection === "Rock") {
+            console.log("You win! " + playerEnter + " beats " +computerSelection);
+    }
+     else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+        console.log("Draw");
+        } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+            console.log("You lose! " + computerSelection + " beats " + playerEnter);
+        } else if (playerSelection === "scissors" && computerSelection === "Paper") { 
+            console.log("You win! " + playerEnter + " beats " +computerSelection);
+    }
+}
+
+
+let playerEnter = prompt ("Rock, Paper or Scissors?");
+playerSelection = playerEnter.toLowerCase();
+const computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerPlay());
+
+console.log(playRound(playerSelection, computerSelection));
+
